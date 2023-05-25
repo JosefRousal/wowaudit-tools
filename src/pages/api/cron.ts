e23ba14806131178ac9651e37eb8bfde2364b8d4
Josef Rousal <josef.crichton@gmail.com>
2023-05-25 02:39:05 -0400

    perhaps

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,9 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "~/server/db";
 
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  
   response.status(200).json({
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     body: request.body,
